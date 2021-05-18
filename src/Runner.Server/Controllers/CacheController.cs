a75fc86771b4e99f7c38092c5933a3bc97300ece
Christopher Homberger <christopher.homberger@web.de>
2021-05-18 13:42:32 +0200

    fix/cache-controller-openid

--- src/Runner.Server/Controllers/CacheController.cs
+++ src/Runner.Server/Controllers/CacheController.cs
@@ -15,6 +15,7 @@ namespace Runner.Server.Controllers {
 
     [ApiController]
     [Route("{owner}/{repo}/_apis/artifactcache")]
+    [Authorize("bearer")]
     public class CacheController : VssControllerBase{
         
         private string _targetFilePath;
@@ -82,8 +83,9 @@ namespace Runner.Server.Controllers {
         }
 
         [HttpGet("get/{filename}")]
+        [AllowAnonymous]
         public FileStreamResult GetCacheEntry(string filename) {   
-            return new FileStreamResult(System.IO.File.OpenRead(System.IO.Path.Combine(_targetFilePath, filename)), "application/octet-stream");
+            return new FileStreamResult(System.IO.File.OpenRead(System.IO.Path.Combine(_targetFilePath, filename)), "application/octet-stream") { EnableRangeProcessing = true };
         }
 
         [HttpPatch("caches/{cacheId}")]
