d45bd17be175e2062f1fde4038f9d7f25a6b5ead
ChristopherHX <christopher.homberger@web.de>
2023-03-10 18:14:44 +0100

    fixup

--- src/Sdk/DTExpressions2/Expressions2/ExpressionParser.cs
+++ src/Sdk/DTExpressions2/Expressions2/ExpressionParser.cs
@@ -424,9 +424,9 @@ namespace GitHub.DistributedTask.Expressions2
             String name,
             out IFunctionInfo functionInfo)
         {
-            return (context.LexicalAnalyzer.Flags & ExpressionFlags.DTExpressionsV1) == ExpressionFlags.None
+            return ((context.LexicalAnalyzer.Flags & ExpressionFlags.DTExpressionsV1) == ExpressionFlags.None
             ? (ExpressionConstants.WellKnownFunctions.TryGetValue(name, out functionInfo) || (context.LexicalAnalyzer.Flags & ExpressionFlags.ExtendedFunctions) != ExpressionFlags.None && ExpressionConstants.AzureWellKnownFunctions.TryGetValue(name, out functionInfo))
-            : (ExpressionConstants.AzureWellKnownFunctions.TryGetValue(name, out functionInfo) || (context.LexicalAnalyzer.Flags & ExpressionFlags.ExtendedFunctions) != ExpressionFlags.None && ExpressionConstants.WellKnownFunctions.TryGetValue(name, out functionInfo)) ||
+            : (ExpressionConstants.AzureWellKnownFunctions.TryGetValue(name, out functionInfo) || (context.LexicalAnalyzer.Flags & ExpressionFlags.ExtendedFunctions) != ExpressionFlags.None && ExpressionConstants.WellKnownFunctions.TryGetValue(name, out functionInfo))) ||
                 context.ExtensionFunctions.TryGetValue(name, out functionInfo);
         }
 
