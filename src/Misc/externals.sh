26eb7d9dd7ac63ae5e815f27d76d6be84b220c5a
ChristopherHX <christopher.homberger@web.de>
2023-08-14 13:19:50 +0000

    .

--- src/Misc/externals.sh
+++ src/Misc/externals.sh
@@ -4,6 +4,7 @@ PRECACHE=$2
 
 NODE_URL=https://nodejs.org/dist
 UNOFFICIAL_NODE_URL=https://unofficial-builds.nodejs.org/download/release
+NODE12_VERSION="12.22.7"
 NODE16_VERSION="16.20.1"
 NODE20_VERSION="20.5.0"
 # used only for win-arm64, remove node16 unofficial version when official version is available
