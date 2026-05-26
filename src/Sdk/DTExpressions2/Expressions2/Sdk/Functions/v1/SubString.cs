01101fa098af8c8c2e217cc4dae73344c287692a
Christopher Homberger <christopher.homberger@web.de>
2026-05-26 17:23:47 +0200

    azp: Add Substring

---
+++ src/Sdk/DTExpressions2/Expressions2/Sdk/Functions/v1/SubString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GitHub.DistributedTask.Expressions2.Sdk.Functions.v1
+{
+    internal sealed class SubString : Function
+    {
+        protected sealed override Boolean TraceFullyRealized => false;
+
+        protected sealed override Object EvaluateCore(EvaluationContext context, out ResultMemory memory)
+        {
+            memory = null;
+            String left = Parameters[0].EvaluateString(context) as String ?? String.Empty;
+            Int32 startIndex = (Int32)Parameters[1].Evaluate(context).ConvertToNumber();
+            Int32 length = (Int32)Parameters[2].Evaluate(context).ConvertToNumber();
+            return left.Substring(startIndex, length);
+        }
+    }
+}
